62610872b5e153a6cc6af5a47c55be22b9bd7e0e
sawada <sawada@csplan.jp>
2021-12-06 12:11:28 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-const {exec} = require('child_process')
-exec('npm run exec testStorage/helloworld.spec.js', (err, stdout, stderr) => {
+const {exec} = require('child_process');
+const { realpath } = require('fs');
+// const express = require('express')
+
+console.log(realpath(__dirname))
+// createProjectDir("projectname", 3)
+/*
+exec('mkdir '+__dirname+'/../testStorage/demomo', (err, stdout, stderr) => {
+// exec( 'ls', (err, stdout, stderr) => {
     console.log({err})
     console.log({stdout})
     console.log({stderr})
 })
+*/
 
 
 function createCommand(projectName) {
     return 'npm run exec testStorage/'+projectName;
+}
+
+function createProjectDir(dirname, id) {
+    exec('mkdir '+realpath(__dirname+'/../testStorage/' + id + '/' + dirname), 
+        (err, stdout, stderr) => {
+            console.log(err);
+            console.log(stdout);
+            console.log(stderr);
+        }
+    )
 }
