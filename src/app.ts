bd95e1275224eaa753b0d9016fc545b77a511a9e
sawada <sawada@csplan.jp>
2021-12-09 23:22:48 +0900

    add aws-adk

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,26 @@ const port = 3000
 
 
 app.get('/:name', (req, res) => {
-    res.send(req.params.name)
+    // res.send(req.params.name)
+    let a = getTestfileFromS3('helo')
+    a.data.name = req.params.name
+    res.send(JSON.stringify(a))
 })
 
 
 app.listen(port, () => {
     console.log('start 3000')
 })
+
+
+interface testFile {
+    name: string;
+    data: any;
+}
+function getTestfileFromS3(testfileName: string): testFile {
+    const testFile: testFile = {
+        name: 'testfile',
+        data: {}
+    }
+    return testFile
+}
