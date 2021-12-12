21ab7166bb923af75132f5e31b99211e00095bfc
sawada <sawada@csplan.jp>
2021-12-12 21:39:09 +0900

    set route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 // const {express} = require('express');
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 3001
 
 
-app.get('/:name', (req, res) => {
+// test section all exec path
+app.get('/exec/:project', (req, res) => {
+  console.log(req.params.project)
+  res.send(JSON.stringify({aa: req.params.project}))
+})
+
+// 所定のセクションのみ実行
+app.get('/exec/:project/:section', (req, res) => {
     // res.send(req.params.name)
     let a = getTestfileFromS3('helo')
-    a.data.name = req.params.name
+    a.data.name = req.params.project
+    a.data.sec = req.params.section
     res.send(JSON.stringify(a))
+})
+
+// project 生成
+app.post('/cerate', (req, res) => {
+  // create S3 baket
 })
 
 
