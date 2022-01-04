d2d3df247e1419ced0901219afb8c18609478e13
sawada <sawada@csplan.jp>
2022-01-04 22:42:23 +0900

    multi login setting

---
+++ main_app/app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Servicies\Sample;
+use App\UseCase\Sample as UseC;
+
+class HomeController extends Controller
+{
+    public function index(Sample $obj, UseC $o)
+    {
+        $obj->callHello();
+        echo '------<br>';
+        $o->invoke();
+    }
+}
---
+++ main_app/resources/views/admin/auth/login.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>hello login</h1>
+
+@endsection
