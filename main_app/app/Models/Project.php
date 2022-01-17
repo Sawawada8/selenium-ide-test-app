4d63b1112f3b80c53e4422feace3b5d7e886ffaf
sawada <sawada@csplan.jp>
2022-01-18 06:58:40 +0900

    add models

---
+++ main_app/app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = ['user_id', 'name'];
+
+    public function sections()
+    {
+        return $this->hasMany(Section::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function labels()
+    {
+        return $this->belongsToMany(Label::class);
+    }
+}
